066909302d36f21e215497b6926cdbc80aed6ca0
MSI\秋吉　翔大 <20217042-akiyoshishota@hcs.ac.jp>
2023-01-31 15:37:30 +0900

    Nextの表示

--- Assets/Scripts/FieldArrayData.cs
+++ Assets/Scripts/FieldArrayData.cs
@@ -1,158 +1,228 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 public class FieldArrayData : MonoBehaviour
 {
-    // タグリストの名前に紐づく番号
-    ///<summary>
-    ///フィールドのオブジェクトリスト
-    /// 0 空欄
-    /// 1 赤いぷよ
-    /// 2 緑のぷよ
-    /// 3 青いぷよ
-    /// 4 黄色いぷよ
-    ///</summary>
-    private int _noBlock = 0;
-    private int _redPuyo = 1;
-    private int _greenPuyo = 2;
-    private int _bluePuyo = 3;
-    private int _yellowPuyo = 4;
     /// <summary>
-    /// 「_Top」はぷよが生成されるポジション
+    /// 「_movePuyoY」はぷよが生成されるポジション
     /// 「_minX」「_minY」「_maxX」「_maxY」で
     /// 動かすぷよの移動させる座標の上限値、下限値を設定
     /// 「_moveX」「_moveY」はConstを経て「_movePuyo」のポジションを変更する変数
     /// 「_deltaX」「_deltaY」はぷよを動かすための座標の加算、減算するための変数
-    /// 「_rotate」は「_movePuy」を回転させるための変数
+    /// 「_rotate」は「_movePuyo」を回転させ、回転による移動の制限をつけるための変数
+    /// 「_puyoFallTime」は経過時間ごとにぷよを自動で落下させるための時間変数
     /// </summary>
-    private int _Top = 6;
-    private int _minX = -3;
-    private int _maxX = 3;
-    private int _minY = -6;
-    private int _maxY = 6;
-    private int _deltaX = 0;
-    private int _deltaY = 0;
-    private int _moveX = 0;
-    private int _moveY = 6;
-    private int _rotate = 0;
+    private float _movePuyoY = 12;
+    private int _movePuyoX = 2;
+    private int _maxX = 6;
+    private int _maxY = 13;
+    private int _outX = 2;
+    private int _outY = 11;
+    private int _nextFirstX = 7;
+    private int _nextFirstY = 10;
+    private int _nextSecondX = 8;
+    private float _nextSecondY = 7.5f;
 
-    private bool _inputState = false;
+    public GameObject[,] _fieldPuyoData;
+    // 縦横の最大数
+    const int FIELD_SIZE_X = 6;
+    const int FIELD_SIZE_Y = 13;
+
     GameObject _movePuyo;
+    GameObject _nextPuyoFirst;
+    GameObject _nextPuyoSecond;
 
-
+    private Text _gameOver = default;
     ///<summary>
     ///動かすふたつのぷよの親になるPrefabを入れる
     ///</summary>
     [Header("動かすぷよの親のPrefabを挿入")]
     [SerializeField] private GameObject _movePuyoPrefab = null;
-    [SerializeField] private GameState _gameState = GameState.START;
-    public GameObject[] blocks;
-
-    ///<summary>
-    ///フィールドデータ用の変数を定義
-    ///</summary>
-    string[] _fieldObjectTagList =
-    {
-        "","RedPuyo","GreenPuyo","BluePuyo","YellowPuyo",
-    };
-    
-    private int[,] _fieldDate =
-    {
-        {0,0,0,0,0,0,},
-    };
-    //縦横の最大値
-    private int _horizontalMaxCount = 12;
-    private int _verticalMaxCount = 6;
+    [SerializeField] private GameObject _textUi = default;
+    public GameObject[] puyos;
+    List<GameObject> checkZumiFieldBlocks = new List<GameObject>();
 
     private void Awake()
     {
-        CleateBlock();
+        StartCleatePuyo();
+        _gameOver = _textUi.GetComponent<Text>();
+        _fieldPuyoData = new GameObject[_maxX, _maxY ];
     }
     private void Update()
     {
-        
-        //ゲーム状態によって処理を分ける
-        switch (_gameState)
+        if( Input.GetKeyDown(KeyCode.R))
         {
-            case GameState.START:
-                SetGameState(GameState.PLAYER);
-                break;
-            case GameState.PLAYER:
-                _deltaX = 0;
-                _deltaY = 0;
-                if (Input.GetKeyDown(KeyCode.RightArrow) && !_inputState)
-                {
-                    _deltaX++;
-                    print(_deltaX);
-                    _inputState = true;
-                }
-                else if(Input.GetKeyDown(KeyCode.LeftArrow) && !_inputState)
-                {
-                    _deltaX--;
-                    print(_deltaX);
-                    _inputState = true;
-                }
-                if (Input.GetKeyDown(KeyCode.UpArrow) && !_inputState)
-                {
-                    _deltaY++;
-                    print(_deltaY);
-                    _inputState = true;
-                }
-                else if (Input.GetKeyDown(KeyCode.DownArrow) && !_inputState)
-                {
-                    _deltaY--;
-                    print(_deltaY);
-                    _inputState = true;
-                }
-                if(Input.GetKeyDown(KeyCode.X))
-                {
-                    _movePuyo.transform.RotateAround(transform.position, new Vector3(0, 0, 1), _rotate + 90);
-                    foreach(Transform child in transform)
-                    {
-                        child.transform.RotateAround(transform.position, new Vector3(0, 0, 1), _rotate - 90);
-                    }
-                    
-                }
-                if (Input.GetKeyDown(KeyCode.Y))
-                {
-                    _movePuyoPrefab.transform.RotateAround(transform.position, new Vector3(0, 0, 1), _rotate - 90);
-                }
-                _moveX = Mathf.Clamp(_moveX + _deltaX, _minX, _maxX);
-                _moveY = Mathf.Clamp(_moveY + _deltaY, _minY, _maxY);
-                MovePuyoPositionChange(_moveX, _moveY);
-                _inputState = false;
-                break;
+            SceneManager.LoadScene("PuyoPuyo");
         }
     }
-    private enum GameState
+    void Hairetu()
     {
-        START, PLAYER, END,
+        for(int x = 0; x < 6; x++)
+        {
+            for(int y=0; y < 10; y++)
+            {
+                GameObject piece = Instantiate(puyos[Random.Range(0, 4)]);
+                piece.transform.position = new Vector3(x, y, 0);
+                _fieldPuyoData[x, y] = piece;
+            }
+        }
     }
-    private void SetGameState(GameState gameState)
+
+    public void Drop()
     {
-        this._gameState = gameState;
+        int nullCount = 0;
+        for (int x = 0; x < 6; x++)
+        {
+            for (int y = 0; y < 13; y++)
+            {
+                if(_fieldPuyoData[x, y] == null)
+                {
+                    nullCount++;
+                }
+                else if (nullCount > 0)
+                {
+                    _fieldPuyoData[x, y].transform.position += new Vector3(0, -nullCount, 0);
+                    _fieldPuyoData[x, y -nullCount] = _fieldPuyoData[x, y];
+                    _fieldPuyoData[x, y] = null;
+                }
+            }
+            nullCount = 0;
+        }
+
+        if (RenketuAri())
+        {
+            StartCoroutine(Erase());
+        }
+        if (!RenketuAri())
+        {
+            if (_fieldPuyoData[_outX, _outY] == null)
+            {
+                CleatePuyo();
+            }
+            else
+            {
+                _gameOver.enabled = true;
+            }
+        }
     }
-    private void MovePuyoPositionChange(int _ChangeX, int _ChangeY)
+
+    bool RenketuAri()
     {
-        _movePuyo.transform.localPosition = new Vector3(_ChangeX, _ChangeY, 0f);
+        for (int x = 0; x < 6; x++)
+        {
+            for (int y = 0; y < 13; y++)
+            {
+                checkZumiFieldBlocks.Clear();
+                if (Renketusuu(x, y, 0) >= 4 && _fieldPuyoData[x, y] != null)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+        IEnumerator Erase()
+    {
+        yield return new WaitForSeconds(0.5f);
+        for (int x = 0; x < 6; x++)
+        {
+            for (int y = 0; y < 13; y++)
+            {
+                checkZumiFieldBlocks.Clear();
+                if(Renketusuu(x, y, 0) >= 4 && _fieldPuyoData[x, y] != null)
+                {
+                    Destroy(_fieldPuyoData[x, y]);
+                }
+            }
+        }
+        yield return new WaitForSeconds(0.5f);
+        Drop();
+    }
+
+    int Renketusuu(int x,int y, int renketusuu)
+    {
+        if(_fieldPuyoData[x, y] == null || checkZumiFieldBlocks.Contains(_fieldPuyoData[x, y]))
+        {
+            return renketusuu;
+        }
+        checkZumiFieldBlocks.Add(_fieldPuyoData[x, y]);
+        renketusuu++;
+        if(x != _maxX - 1 && _fieldPuyoData[x + 1, y] != null && _fieldPuyoData[x, y].name == _fieldPuyoData[x + 1, y].name)
+        {
+            renketusuu = Renketusuu(x + 1, y, renketusuu);
+        }
+        if (x != 0 && _fieldPuyoData[x - 1, y] != null && _fieldPuyoData[x, y].name == _fieldPuyoData[x - 1, y].name)
+        {
+            renketusuu = Renketusuu(x - 1, y, renketusuu);
+        }
+        if (y != 0 && _fieldPuyoData[x, y - 1] != null && _fieldPuyoData[x, y].name == _fieldPuyoData[x, y - 1].name)
+        {
+            renketusuu = Renketusuu(x, y - 1, renketusuu);
+        }
+        if (y != _maxY - 1 && _fieldPuyoData[x, y + 1] != null && _fieldPuyoData[x, y].name == _fieldPuyoData[x, y + 1].name)
+        {
+            renketusuu = Renketusuu(x, y + 1, renketusuu);
+        }
+        return renketusuu;
     }
     /// <summary>
     /// 操作するぷよの
     /// 「_headPuyo」=上のぷよ
     /// 「_tailPuyo」=下のぷよ
     /// 「_movePuyo」=操作する2つのぷよ
+    /// をGameObject「Stage」の子として生成
     /// </summary>
-    private void CleateBlock()
+    public void StartCleatePuyo()
     {
-        _movePuyo = Instantiate(_movePuyoPrefab);
-        _movePuyo.transform.position = new Vector2(0, _Top);
-        GameObject _headPuyo = Instantiate(blocks[Random.Range(0, 4)]);
-        _headPuyo.transform.position = new Vector2(0, _Top);
+        _movePuyo=Instantiate(_movePuyoPrefab);
+        _movePuyo.transform.position = new Vector2(_movePuyoX, _movePuyoY);
+        GameObject _headPuyo = Instantiate(puyos[Random.Range(0, 4)]);
+        _headPuyo.transform.position = new Vector2(_movePuyoX, _movePuyoY);
         _headPuyo.transform.SetParent(_movePuyo.transform, true);
-        GameObject _tailPuyo = Instantiate(blocks[Random.Range(0, 4)]);
-        _tailPuyo.transform.position = new Vector2(0, _Top-1);
+        GameObject _tailPuyo = Instantiate(puyos[Random.Range(0, 4)]);
+        _tailPuyo.transform.position = new Vector2(_movePuyoX, _movePuyoY - 1);
         _tailPuyo.transform.SetParent(_movePuyo.transform, true);
+
+        _nextPuyoFirst = Instantiate(_movePuyoPrefab);
+        _nextPuyoFirst.GetComponent<MovePuyo>().enabled = false;
+        _nextPuyoFirst.transform.position = new Vector2(_nextFirstX, _nextFirstY);
+        GameObject _nextPuyoFirstHead = Instantiate(puyos[Random.Range(0, 4)]);
+        _nextPuyoFirstHead.transform.position = new Vector2(_nextFirstX, _nextFirstY);
+        _nextPuyoFirstHead.transform.SetParent(_nextPuyoFirst.transform, true);
+        GameObject _nextPuyoFirstTail = Instantiate(puyos[Random.Range(0, 4)]);
+        _nextPuyoFirstTail.transform.position = new Vector2(_nextFirstX, _nextFirstY - 1);
+        _nextPuyoFirstTail.transform.SetParent(_nextPuyoFirst.transform, true);
+
+        _nextPuyoSecond = Instantiate(_movePuyoPrefab);
+        _nextPuyoSecond.GetComponent<MovePuyo>().enabled = false;
+        _nextPuyoSecond.transform.position = new Vector2(_nextSecondX, _nextSecondY);
+        GameObject _nextPuyoSecondHead = Instantiate(puyos[Random.Range(0, 4)]);
+        _nextPuyoSecondHead.transform.position = new Vector2(_nextSecondX, _nextSecondY);
+        _nextPuyoSecondHead.transform.SetParent(_nextPuyoSecond.transform, true);
+        GameObject _nextPuyoSecondTail = Instantiate(puyos[Random.Range(0, 4)]);
+        _nextPuyoSecondTail.transform.position = new Vector2(_nextSecondX, _nextSecondY - 1);
+        _nextPuyoSecondTail.transform.SetParent(_nextPuyoSecond.transform, true);
     }
+    public void CleatePuyo()
+    {
+        _movePuyo = _nextPuyoFirst;
+        _movePuyo.transform.position = new Vector2(_movePuyoX, _movePuyoY);
+        _nextPuyoFirst.GetComponent<MovePuyo>().enabled = true;
     
+        _nextPuyoFirst = _nextPuyoSecond;
+        _nextPuyoFirst.transform.position = new Vector2(_nextFirstX, _nextFirstY);
+    
+        _nextPuyoSecond = Instantiate(_movePuyoPrefab);
+        _nextPuyoSecond.GetComponent<MovePuyo>().enabled = false;
+        _nextPuyoSecond.transform.position = new Vector2(_nextSecondX, _nextSecondY);
+        GameObject _nextPuyoSecondHead = Instantiate(puyos[Random.Range(0, 4)]);
+        _nextPuyoSecondHead.transform.position = new Vector2(_nextSecondX, _nextSecondY);
+        _nextPuyoSecondHead.transform.SetParent(_nextPuyoSecond.transform, true);
+        GameObject _nextPuyoSecondTail = Instantiate(puyos[Random.Range(0, 4)]);
+        _nextPuyoSecondTail.transform.position = new Vector2(_nextSecondX, _nextSecondY - 1);
+        _nextPuyoSecondTail.transform.SetParent(_nextPuyoSecond.transform, true);
+    }
 }
--- Assets/Scripts/MovePuyo.cs
+++ Assets/Scripts/MovePuyo.cs
@@ -5,49 +5,140 @@ using UnityEngine;
 public class MovePuyo : MonoBehaviour
 {
     private int _moveX = 0;
-    private int _moveY = 0;
-    private int _minX = -3;
-    private int _maxX = 3;
-    private int _minY = -6;
-    private int _maxY = 6;
-    private bool _inputState = false;
-    private Transform _thisTransform = null;
+    private float _moveY = 6;
+    private int _minX = 0;
+    private int _maxX = 6;
+    private float _minY = 0;
+    private int _maxY = 13;
+    private float _puyoFallTime = 0;
 
-    [SerializeField] private GameState _gameState = GameState.START;
+    GameObject _headPuyo;
+    GameObject _tailPuyo;
+
+    FieldArrayData GameControl;
     /// <summary>
     /// 「_minX」「_minY」「_maxX」「_maxY」で
     /// 動かすぷよの移動させる座標の上限値、下限値を設定
     /// </summary>
     private void Start()
     {
-        //このオブジェクトのTransformコンポーネント取得
-        Transform _thisTransform = this.GetComponent<Transform>();
+      _headPuyo = transform.GetChild(0).gameObject;
+      _tailPuyo = transform.GetChild(1).gameObject; 
     }
 
     private void Update()
     {
-        //ゲーム状態によって処理を分ける
-        switch (_gameState)
+        ///<summary>
+        ///一秒経過するごとに自動的に1マス分落下する
+        ///</summary>
+        _puyoFallTime += Time.deltaTime;
+        if (_puyoFallTime > 1)
         {
-            case GameState.START:
-                SetGameState(GameState.PLAYER);
-                break;
-            case GameState.PLAYER:
-                if (Input.GetKeyDown(KeyCode.LeftArrow) && !_inputState)
+            this.gameObject.transform.position += new Vector3(0, -1, 0);
+            if (!CanMove())
+            {
+                this.gameObject.transform.position += new Vector3(0, 1, 0);
+                LandingPuyo();
+                this.gameObject.transform.DetachChildren();
+                FindObjectOfType<FieldArrayData>().Drop();
+                Destroy(this.gameObject, 10f);
+                this.enabled = false;
+            }
+            _puyoFallTime = 0;
+        }
+        //「→」キーを押すと右に移動する
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            this.gameObject.transform.position += new Vector3(1, 0, 0);
+            if (!CanMove())
+            {
+                this.gameObject.transform.position += new Vector3(-1, 0, 0);
+            }            
+        }
+        //「←」キーを押すと左に移動する
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            this.gameObject.transform.position += new Vector3(-1, 0, 0);
+            if (!CanMove())
+            {
+                this.gameObject.transform.position += new Vector3(1, 0, 0);
+            }
+        }
+        //「↓」キーを押すと下に移動する
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            this.gameObject.transform.position += new Vector3(0, -1, 0);
+            if (!CanMove())
+            {
+                this.gameObject.transform.position += new Vector3(0, 1, 0);
+            }
+            else
+            {
+                _puyoFallTime = 0;
+            }
+        }
+        //「z」キーを押すと左方向に回転する
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            this.gameObject.transform.RotateAround(transform.position, new Vector3(0, 0, 1), 90);
+            foreach (Transform childPuyo in transform)
+            {
+                childPuyo.transform.RotateAround(childPuyo.transform.position, new Vector3(0, 0, 1), -90);
+            }
+            if (!CanMove())
+            {
+                this.gameObject.transform.RotateAround(transform.position, new Vector3(0, 0, 1), -90);
+                foreach (Transform childPuyo in transform)
                 {
-                    
+                    childPuyo.transform.RotateAround(childPuyo.transform.position, new Vector3(0, 0, 1), 90);
                 }
-                //Transform _thisTransform = 
-                    break;
+            }
+        }
+        //「x」キーを押すと右方向に回転する
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            this.gameObject.transform.RotateAround(transform.position, new Vector3(0, 0, 1), -90);
+            foreach (Transform childPuyo in transform)
+            {
+                childPuyo.transform.RotateAround(childPuyo.transform.position, new Vector3(0, 0, 1), 90);
+            }
+            if (!CanMove())
+            {
+                this.gameObject.transform.RotateAround(transform.position, new Vector3(0, 0, 1), 90);
+                foreach (Transform childPuyo in transform)
+                {
+                    childPuyo.transform.RotateAround(childPuyo.transform.position, new Vector3(0, 0, 1), -90);
+                }
+            }
         }
     }
 
-    private enum GameState
+    bool CanMove()
     {
-        START, PLAYER, END,
+        foreach (Transform childPuyo in transform)
+        {
+            int _childX = Mathf.RoundToInt(childPuyo.transform.position.x);
+            int _childY = Mathf.RoundToInt(childPuyo.transform.position.y);
+
+            if ( _childX < _minX || _childX >= _maxX || _childY < _minY)
+            {
+                return false;               
+            }
+            if(FindObjectOfType<FieldArrayData>()._fieldPuyoData[_childX, _childY] != null)
+            {
+                return false;
+            }
+        }
+        return true;
     }
-    private void SetGameState(GameState gameState)
+    public void LandingPuyo()
     {
-        this._gameState = gameState;
+        foreach(Transform ChildPuyo in transform)
+        {
+            int _childX = Mathf.RoundToInt(ChildPuyo.transform.position.x);
+            int _childY = Mathf.RoundToInt(ChildPuyo.transform.position.y);
+            FindObjectOfType<FieldArrayData>()._fieldPuyoData[_childX, _childY] = ChildPuyo.gameObject;
+        }
+        
     }
 }
